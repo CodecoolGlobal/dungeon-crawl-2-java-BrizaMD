418461e5e72f7b4e05755d978890ca21847e9064
kovacsalex <kovacsalex95@gmail.com>
2021-06-22 10:14:57 +0200

    camel case fixed

--- sample_data/custom_schema.sql
+++ sample_data/custom_schema.sql
@@ -9,49 +9,49 @@ CREATE TABLE public.saves (
 DROP TABLE IF EXISTS public.player;
 CREATE TABLE public.player (
    id serial NOT NULL PRIMARY KEY,
-   saveID integer NOT NULL,
+   save_id integer NOT NULL,
    hp integer NOT NULL,
-   maximumHP integer NOT NULL,
+   maximum_hp integer NOT NULL,
    armor integer NOT NULL,
    x integer NOT NULL,
    y integer NOT NULL,
-   selectedItem integer NOT NULL,
-   hasRedKey boolean NOT NULL,
-   hasBlueKey boolean NOT NULL
+   selected_item integer NOT NULL,
+   has_red_key boolean NOT NULL,
+   has_blue_key boolean NOT NULL
 );
 
 DROP TABLE IF EXISTS public.playerInventory;
 CREATE TABLE public.playerInventory (
     id serial NOT NULL PRIMARY KEY,
-    saveID integer NOT NULL,
-    tileName varchar(255) NOT NULL
+    save_id integer NOT NULL,
+    tile_name varchar(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS public.enemies;
 CREATE TABLE public.enemies (
     id serial NOT NULL PRIMARY KEY,
-    saveID integer NOT NULL,
+    save_id integer NOT NULL,
     x integer NOT NULL,
     y integer NOT NULL,
-    tileName varchar(255) NOT NULL
+    tile_name varchar(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS public.pickedItems;
 CREATE TABLE public.pickedItems (
     id serial NOT NULL PRIMARY KEY,
-    saveID integer NOT NULL,
+    save_id integer NOT NULL,
     x integer NOT NULL,
     y integer NOT NULL
 );
 
 ALTER TABLE ONLY public.player
-    ADD CONSTRAINT fk_save_id FOREIGN KEY (saveID) REFERENCES public.saves(id);
+    ADD CONSTRAINT fk_save_id FOREIGN KEY (save_id) REFERENCES public.saves(id);
 
 ALTER TABLE ONLY public.playerInventory
-    ADD CONSTRAINT fk_save_id FOREIGN KEY (saveID) REFERENCES public.saves(id);
+    ADD CONSTRAINT fk_save_id FOREIGN KEY (save_id) REFERENCES public.saves(id);
 
 ALTER TABLE ONLY public.enemies
-    ADD CONSTRAINT fk_save_id FOREIGN KEY (saveID) REFERENCES public.saves(id);
+    ADD CONSTRAINT fk_save_id FOREIGN KEY (save_id) REFERENCES public.saves(id);
 
 ALTER TABLE ONLY public.pickedItems
-    ADD CONSTRAINT fk_save_id FOREIGN KEY (saveID) REFERENCES public.saves(id);
+    ADD CONSTRAINT fk_save_id FOREIGN KEY (save_id) REFERENCES public.saves(id);
